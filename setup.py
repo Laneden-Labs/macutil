eeaa96f60b927cbb956031fc4da47806b242f437
Darryl Lane <darryllane101@gmail.com>
2021-11-14 23:16:40 +0000

    pypi refactor

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 import setuptools
 setup(
     name='macutil',
-    version='0.1.8',
+    version='0.1.19',
     author='Darryl lane',
     author_email='Darryllane101@gmail.com',
     url='https://github.com/Laneden-Labs/macutil',
-    packages=setuptools.find_packages(),
+    packages=['Package'],
     include_package_data=True,
     license='LICENSE.txt',
     description='''MAC Address brute forcing tool, used to bypass MAC based filtering''',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
-    scripts=['macutil'],
+    scripts=['Package/macutil']
 )
+
 
