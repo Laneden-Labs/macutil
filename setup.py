8399a5c18b4717f08a72103127abbc0213d5adc0
Darryl Lane <darryllane101@gmail.com>
2022-02-25 23:23:26 +0000

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import setuptools
 setup(
     name='macutil',
-    version='0.1.20',
+    version='0.1.21',
     author='Darryl lane',
     author_email='Darryllane101@gmail.com',
     url='https://github.com/Laneden-Labs/macutil',
